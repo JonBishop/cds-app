cc184f3045a44bcba1ea23ec2d39ae9e090150ce
JonBishop <jonpbishop@hotmail.com>
2024-01-17 21:51:00 +0000

    Changed plasmic project to Creative Development Studio

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,14 @@
 export async function load() {
 	
     const response = await fetch(
-        'https://codegen.plasmic.app/api/v1/loader/html/preview/wMvSWG6XGxYCG3u4uhDw8j/Homepage?hydrate=1&embedHydrate=1',
-        {
-          headers: {
-            // Your project ID and public API token
-            'x-plasmic-api-project-tokens': 'wMvSWG6XGxYCG3u4uhDw8j:UL6Lg0UFnDoypdXN8uuoimYkjuFqCbezRqgLaSH2kTGmZCYZwxhSOG6u1YXJDyC7aLzkC1oU84AqAmtDx5Yx0A'
-            // You can find your PROJECT token by visiting
-            // https://studio.plasmic.app/projects/[wMvSWG6XGxYCG3u4uhDw8j]/docs/loader#showToken=true
-          }
+      'https://codegen.plasmic.app/api/v1/loader/html/preview/rupsZDhVzjZ1Fck6Tv5ThW/Homepage?hydrate=1&embedHydrate=1',
+      {
+        headers: {
+          // Your project ID and public API token
+          'x-plasmic-api-project-tokens': 'rupsZDhVzjZ1Fck6Tv5ThW:i6v5GS8LZyPtvu2AHXn9UCbZTmrlg1VNTbvUK5pXRLdZc708Nvm7rv8UeLUgIZAVLfSzaHbFCybDhsBA'
+          // You can find your PROJECT token by visiting
+          // https://studio.plasmic.app/projects/[rupsZDhVzjZ1Fck6Tv5ThW]/docs/loader#showToken=true
+        }
         }
       );
       const result = await response.json();
